2a843bd5ed8b98a48bdad4348459343f1992f4eb
Victor Guerra <vguerra@gmail.com>
2017-10-01 15:08:22 +0200

    adding reg term to loss function

--- main.py
+++ main.py
@@ -103,8 +103,12 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
 
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
 
+    #regularization
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    print(reg_losses)
+    
     cross_entropy_loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label) + 1e-3*sum(reg_losses))
 
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
                                        name='Adam')
